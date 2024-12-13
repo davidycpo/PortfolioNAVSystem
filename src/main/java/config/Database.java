78efc8c7937e7512b4d0ac49acd1664f4a0bd89f
David Po <davidycpo@gmail.com>
2024-12-13 20:31:02 +0800

    asset model embedded SQLite db

---
+++ src/main/java/config/Database.java
@@ -0,0 +1,78 @@
+package config;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import model.Asset;
+import model.AssetType;
+import utils.Utils;
+
+public class Database {
+	private final Connection connection;
+
+	public Database(String url) throws SQLException {
+		connection = DriverManager.getConnection(url);
+		initializeDatabase();
+	}
+
+	private void initializeDatabase() throws SQLException {
+		System.out.println("Initializing database...");
+		String createTableSQL = "CREATE TABLE IF NOT EXISTS assets (" + "ticker TEXT PRIMARY KEY,"
+				+ "assetType TEXT NOT NULL," + "strike REAL," + "maturityDate REAL" + ");";
+		try (Statement statement = connection.createStatement()) {
+			statement.execute(createTableSQL);
+		}
+		System.out.println("Finished initializing database...");
+	}
+
+	public void insertAsset(Asset asset) throws SQLException {
+		if (Utils.isBlank(asset.getTicker())) {
+			throw new IllegalArgumentException("Asset ticker is required");
+		}
+		if (asset.getAssetType() == null) {
+			throw new IllegalArgumentException("Asset Type is required");
+		}
+
+		if (asset.getAssetType() == AssetType.CALL_OPTION || asset.getAssetType() == AssetType.PUT_OPTION) {
+			if (asset.getStrike() == 0d) {
+				throw new IllegalArgumentException("Strike is required");
+			}
+			if (asset.getMaturityDate() == null) {
+				throw new IllegalArgumentException("Maturity Date is required");
+			}
+		}
+
+		System.out.println("Inserting Asset");
+
+		String insertSQL = "INSERT INTO assets (ticker, assetType, strike, maturityDate) VALUES (?, ?, ? , ?)";
+		try (PreparedStatement statement = connection.prepareStatement(insertSQL)) {
+			statement.setString(1, asset.getTicker());
+			statement.setString(2, asset.getAssetType().toString());
+			statement.setDouble(3, asset.getStrike());
+			statement.setDate(4, asset.getMaturityDate());
+			statement.execute();
+		}
+
+		System.out.println("Inserted Asset");
+	}
+
+	public Asset getAsset(final String ticker) throws SQLException {
+		String selectSQL = "SELECT * FROM assets WHERE ticker = ?";
+		try (PreparedStatement statement = connection.prepareStatement(selectSQL)) {
+			statement.setString(1, ticker);
+			ResultSet rs = statement.executeQuery();
+			return new Asset(rs.getString(1), AssetType.valueOf(rs.getString(2)), rs.getDouble(3), rs.getDate(4));
+		}
+	}
+
+	// Close the connection when done
+	public void close() throws SQLException {
+		if (connection != null) {
+			connection.close();
+		}
+	}
+}
---
+++ src/main/java/utils/Utils.java
@@ -0,0 +1,7 @@
+package utils;
+
+public class Utils {
+	public static boolean isBlank(String str) {
+		return str == null || str.trim().isEmpty();
+	}
+}
